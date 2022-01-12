3ab683d1cb2d1681d649ef2e4c6b763dee1a532a
Gurmukh Hare <gurmukhhare@Gurmukhs-MacBook-Pro.local>
2022-01-11 21:54:18 -0800

    updating database configuration to connect to heroku postgres

--- routes/inventory.js
+++ routes/inventory.js
@@ -7,10 +7,8 @@ const { check,validationResult } = require('express-validator');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'gurmukhhare',
-    password : '',
-    database : 'shopify-backend-db'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
